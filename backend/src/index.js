904da296bb292088d40d97785e99b988005e78b2
Sahil Mishra <sahil.mishra1512@email.com>
2025-04-01 04:17:41 +0530

    Real Time Chat App

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.route.js"
-
+import cors from "cors"
 
 dotenv.config();
 const app = express();
@@ -14,7 +14,10 @@ const PORT= process.env.PORT;
 
 app.use(express.json()); // extract json data from the body.(Has to be before routes)
 app.use(cookieParser()); // Parse the cookie
-
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}))
 
 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, {useEffect} from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import LoginPage from "./pages/LoginPage";
+import { useAuthStore } from "./store/useAuthStore";
+
 
 function App() {
+
+  const { authUser, checkAuth } = useAuthStore();
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+
+
   return (
     <div>
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 }
 
-export default App
+export default App;
